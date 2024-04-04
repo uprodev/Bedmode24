5a25e233f0a56059e0ac363a6130668477c21ad3
Stanislav-pr <stanislav.retinskiy@gmail.com>
2024-04-04 23:18:56 +0200

    Create image_and_video_slider.php

---
+++ wp-theme/bedmode24/templates/sections/blocks/image_and_video_slider.php
@@ -0,0 +1,39 @@
+<?php
+
+
+?>
+
+<section class="slider-block bg-light-grey">
+    <div class="container">
+        <div class="row">
+            <h6 class="subtitle">Subtitle</h6>
+            <h2 class="title">H2 Video of afbeelding titel</h2>
+            <div class="slider-wrap p-0">
+                <div class="swiper img-slider">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <figure>
+                                <img src="img/img-11.jpg" alt="">
+                            </figure>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="video-block">
+                                <a data-fancybox href="https://www.youtube.com/watch?v=_sI_Ps7JSEk">
+                                    <img src="img/img-11.jpg" alt="">
+                                    <span class="icon-wrap">
+												<i class="fal fa-play-circle"></i>
+											</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-pagination img-pagination"></div>
+                </div>
+                <div class="nav-wrap">
+                    <div class="swiper-button-next img-next"><i class="fal fa-arrow-circle-right"></i></div>
+                    <div class="swiper-button-prev img-prev"><i class="fal fa-arrow-circle-left"></i></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
