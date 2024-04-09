6f5bb9bc2233e13a84e3880f0e7ab267a94fe8cd
Stanislav-pr <stanislav.retinskiy@gmail.com>
2024-04-09 13:35:54 +0200

    Update content-single-product.php

--- wp-theme/bedmode24/woocommerce/content-single-product.php
+++ wp-theme/bedmode24/woocommerce/content-single-product.php
@@ -172,14 +172,6 @@ $attributes = $product->get_attributes();
     </div>
 </section>
 
-	<?php
-	/**
-	 * Hook: woocommerce_after_single_product_summary.
-	 *
-	 * @hooked woocommerce_output_product_data_tabs - 10
-	 * @hooked woocommerce_upsell_display - 15
-	 * @hooked woocommerce_output_related_products - 20
-	 */
+<?php woocommerce_upsell_display();?>
 
-	?>
-
+<?php woocommerce_output_related_products();
