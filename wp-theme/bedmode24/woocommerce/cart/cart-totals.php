7b0081923373916a48de8a162f0ea369e6e16696
Stanislav-pr <stanislav.retinskiy@gmail.com>
2024-04-10 15:20:36 +0200

    Update cart-totals.php

--- wp-theme/bedmode24/woocommerce/cart/cart-totals.php
+++ wp-theme/bedmode24/woocommerce/cart/cart-totals.php
@@ -38,48 +38,45 @@ $total_price = get_field('total_price', $idc);
                 <span>(Inclusief 13,87 BTW 21%)</span>
             </p>
         </li>
+
+        <?php
+        if ( wc_tax_enabled() && ! WC()->cart->display_prices_including_tax() ) {
+            $taxable_address = WC()->customer->get_taxable_address();
+            $estimated_text  = '';
+
+            if ( WC()->customer->is_customer_outside_base() && ! WC()->customer->has_calculated_shipping() ) {
+                /* translators: %s location. */
+                $estimated_text = sprintf( ' <small>' . esc_html__( '(estimated for %s)', 'woocommerce' ) . '</small>', WC()->countries->estimated_for_prefix( $taxable_address[0] ) . WC()->countries->countries[ $taxable_address[0] ] );
+            }
+
+            if ( 'itemized' === get_option( 'woocommerce_tax_total_display' ) ) {
+                foreach ( WC()->cart->get_tax_totals() as $code => $tax ) { // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
+                    ?>
+                    <li class="tax-rate tax-rate-<?php echo esc_attr( sanitize_title( $code ) ); ?>">
+                        <p><?php echo esc_html( $tax->label ) . $estimated_text; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+                        <p data-title="<?php echo esc_attr( $tax->label ); ?>"><?php echo wp_kses_post( $tax->formatted_amount ); ?></p>
+                    </li>
+                    <?php
+                }
+            } else {
+                ?>
+                <li class="tax-total">
+                    <p><?php echo esc_html( WC()->countries->tax_or_vat() ) . $estimated_text; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+                    <p data-title="<?php echo esc_attr( WC()->countries->tax_or_vat() ); ?>"><?php wc_cart_totals_taxes_total_html(); ?></p>
+                </li>
+                <?php
+            }
+        }
+        ?>
+
+
         <li>
             <?php do_action( 'woocommerce_proceed_to_checkout' ); ?>
         </li>
     </ul>
 
 
-
-		<?php
-		if ( wc_tax_enabled() && ! WC()->cart->display_prices_including_tax() ) {
-			$taxable_address = WC()->customer->get_taxable_address();
-			$estimated_text  = '';
-
-			if ( WC()->customer->is_customer_outside_base() && ! WC()->customer->has_calculated_shipping() ) {
-				/* translators: %s location. */
-				$estimated_text = sprintf( ' <small>' . esc_html__( '(estimated for %s)', 'woocommerce' ) . '</small>', WC()->countries->estimated_for_prefix( $taxable_address[0] ) . WC()->countries->countries[ $taxable_address[0] ] );
-			}
-
-			if ( 'itemized' === get_option( 'woocommerce_tax_total_display' ) ) {
-				foreach ( WC()->cart->get_tax_totals() as $code => $tax ) { // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
-					?>
-					<tr class="tax-rate tax-rate-<?php echo esc_attr( sanitize_title( $code ) ); ?>">
-						<th><?php echo esc_html( $tax->label ) . $estimated_text; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></th>
-						<td data-title="<?php echo esc_attr( $tax->label ); ?>"><?php echo wp_kses_post( $tax->formatted_amount ); ?></td>
-					</tr>
-					<?php
-				}
-			} else {
-				?>
-				<tr class="tax-total">
-					<th><?php echo esc_html( WC()->countries->tax_or_vat() ) . $estimated_text; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></th>
-					<td data-title="<?php echo esc_attr( WC()->countries->tax_or_vat() ); ?>"><?php wc_cart_totals_taxes_total_html(); ?></td>
-				</tr>
-				<?php
-			}
-		}
-		?>
-
-
 		<?php do_action( 'woocommerce_cart_totals_after_order_total' ); ?>
-
-
-
 
 
 	<?php do_action( 'woocommerce_after_cart_totals' ); ?>
