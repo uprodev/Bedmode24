8e345f0c72aa31686c05f83ffb5d86eb85e36521
Stanislav-pr <stanislav.retinskiy@gmail.com>
2024-04-11 23:20:03 +0200

    Update archive-product.php

--- wp-theme/bedmode24/woocommerce/archive-product.php
+++ wp-theme/bedmode24/woocommerce/archive-product.php
@@ -21,6 +21,7 @@ get_header();
 
 if(is_shop()){
     $ids = wc_get_page_id( 'shop' );
+    $idcat = wc_get_page_id( 'shop' );
     $title = get_field('title', $ids);
     $name = get_the_title($ids);
     $link = get_field('optional_white_button', $ids);
@@ -28,6 +29,7 @@ if(is_shop()){
     $text = get_field('text', $ids);
 }else{
     $ids = get_queried_object()->term_id;
+    $idcat = 'product_cat_'.get_queried_object()->term_id;
     $name = get_queried_object()->name;
     $title = get_field('title', 'product_cat_'.$ids);
     $link = get_field('optional_white_button', 'product_cat_'.$ids);
@@ -112,58 +114,15 @@ if(is_shop()){
         </div>
     </section>
 
-    <section class="text-section text-section-2 ">
-        <div class="container">
-            <div class="row">
-                <div class="content">
-                    <h6 class="subtitle">Subtitle</h6>
-                    <h2 class="title">H2 Text heading</h2>
-                    <div class="wrap">
-                        <p>Nulla vitae elit libero, a pharetra augue. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
 
-                        <p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Curabitur blandit tempus porttitor. Maecenas faucibus mollis interdum. Nulla vitae elit libero, a pharetra augue. Etiam porta sem malesuada magna mollis euismod.</p>
-                        <p>Nulla vitae elit libero, a pharetra augue. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+    <?php if( have_rows('content', $idcat) ): ?>
 
-                        <p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Lorem ipsum dolor sit amet, <a href="#">consectetur adipiscing elit</a>. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Curabitur blandit tempus porttitor. Maecenas faucibus mollis interdum. Nulla vitae elit libero, a pharetra augue. Etiam porta sem malesuada magna mollis euismod.</p>
-                    </div>
-                    <div class="btn-wrap d-flex justify-content-center">
-                        <a href="#" class="btn-default btn-blue btn-shadow px-5 rounded-5">Call to action</a>
-                    </div>
-                </div>
+        <?php while( have_rows('content', $idcat) ): the_row(); ?>
 
+            <?php get_template_part('templates/sections/blocks/' . get_row_layout()); ?>
 
-            </div>
-        </div>
-    </section>
+        <?php endwhile; ?>
 
-    <section class="text-default bg-light-grey">
-        <div class="container">
-            <div class="row justify-content-between">
-                <div class="content col-md-6 col-12 m-auto p-0">
-                    <h2>H2 Vestibulum id ligula porta felis euismod semper</h2>
-                    <p>Sed posuere consectetur est at lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper nulla non metus auctor fringilla.</p>
-
-                    <p>Maecenas sed diam eget risus varius blandit sit amet non magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper nulla non metus auctor fringilla.</p>
-                    <h3>H3 Donec sed odio dui</h3>
-                    <p>Sed posuere consectetur est at lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper nulla non metus auctor fringilla.</p>
-                    <h3>H3 Hier een opsomming</h3>
-                    <ul>
-                        <li><a href="#">Hier ruimte voor een stuk tekst</a></li>
-                        <li>Lorem ipsum dolor sit amet, consectetue voor een stuk tekst</li>
-                        <li>Hier ruimte voor een stuk tekst</li>
-                        <li>Lorem ipsum dolor sit amet, consectetuor een stuk tekst</li>
-                        <li>Hier ruimte voor een stuk tekst</li>
-                    </ul>
-
-                    <figure>
-                        <img src="img/img-14.jpg" alt="">
-                    </figure>
-
-                    <h3>H3 Donec sed odio dui</h3>
-                    <p>Sed posuere consectetur est at lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper nulla non metus auctor fringilla.</p>
-                </div>
-            </div>
-        </div>
-    </section>
+    <?php endif; ?>
 
 <?php get_footer();
