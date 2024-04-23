678aa9e29d2ee23d1d164b08706b007a32bcc98f
Stanislav-pr <stanislav.retinskiy@gmail.com>
2024-04-23 21:25:32 +0200

    Update merken_product_overview.php

--- wp-theme/bedmode24/templates/sections/blocks/merken_product_overview.php
+++ wp-theme/bedmode24/templates/sections/blocks/merken_product_overview.php
@@ -4,6 +4,39 @@ $default= get_sub_field('custom__default_product_selection');
 $custom= get_sub_field('product_selector');
 $merks = get_sub_field('product_eigenschappen_selector');
 
+$catalog_orderby_options = apply_filters(
+    'woocommerce_catalog_orderby',
+    array(
+        'popularity' => __( 'Populariteit', 'bedmode24' ),
+        'price'      => __( 'Prijs (oplopend)', 'bedmode24' ),
+        'price-desc' => __( 'Prijs (aflopend)', 'bedmode24' ),
+    )
+);
+$orderby = isset( $_GET['orderby'] ) ? wc_clean( wp_unslash( $_GET['orderby'] ) ) : '';
+
+
+if($default) {
+
+    $paged = get_query_var('paged') ? get_query_var('paged') : 1;
+    $args = [
+        'post_type' => 'product',
+        'posts_per_page' => 12,
+        'paged' => $paged,
+        'tax_query' => [
+            [
+                'taxonomy' => 'pa_merk',
+                'field' => 'id',
+                'terms' => $merks
+            ]
+        ]
+    ];
+    $args = berocket_filter_query_vars_hook($args);
+
+    $wp_query = new WP_Query($args);
+
+    $total = $wp_query->found_posts;
+
+}
 ?>
 
 <section class="product-content bg-light-grey pt-5">
@@ -23,31 +56,28 @@ $merks = get_sub_field('product_eigenschappen_selector');
                             <a href="#"><i class="fal fa-filter"></i> Filter</a>
                         </div>
                         <div class="filter-select d-flex flex-wrap">
-                            <?php woocommerce_result_count();?>
+                            <p class="woocommerce-result-count">
+
+                                <?php printf( _n( '%d product gevonden', '%d producten gevonden', $total, 'woocommerce' ), $total );
+                                ?>
+                            </p>
                             <?= do_shortcode('[br_filters_group group_id=428]');?>
                         </div>
-                        <?php woocommerce_catalog_ordering();?>
+                        <form class="woocommerce-ordering select-block" method="get">
+                            <label class="form-label" for="orderby"></label>
+                            <select name="orderby" class="orderby" aria-label="<?php esc_attr_e( 'Shop order', 'woocommerce' ); ?>">
+                                <option value="0" selected disabled>Sorteren...</option>
+                                <?php foreach ( $catalog_orderby_options as $id => $name ) : ?>
+                                    <option value="<?php echo esc_attr( $id ); ?>" <?php selected( $orderby, $id ); ?>><?php echo esc_html( $name ); ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                            <input type="hidden" name="paged" value="1" />
+                            <?php wc_query_string_form_fields( null, array( 'orderby', 'submit', 'paged', 'product-page' ) ); ?>
+                        </form>
                     </div>
                     <div class="sorted-content d-flex flex-wrap">
 
                         <?php if($default):
-                            
-                            $paged = get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1;
-                            $args = [
-                                'post_type' => 'product',
-                                'posts_per_page' => 12,
-                                'paged' => $paged,
-                                'tax_query' => [
-                                    [
-                                        'taxonomy' => 'pa_merk',
-                                        'field' => 'id',
-                                        'terms' => $merks
-                                    ]
-                                ]
-                            ];
-                            $args = berocket_filter_query_vars_hook($args);
-
-                            $wp_query = new WP_Query($args);
 
                             while($wp_query->have_posts()): $wp_query->the_post();?>
 
