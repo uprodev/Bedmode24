a71aa07f08af1267e48b5fbead93ab437ffd46c4
Stanislav-pr <stanislav.retinskiy@gmail.com>
2024-04-24 22:37:13 +0200

    Create thank_you.php

---
+++ wp-theme/bedmode24/templates/sections/blocks/thank_you.php
@@ -0,0 +1,55 @@
+<?php
+
+$img = get_field('background_image');
+$subtitle = get_field('subtitle');
+$title = get_field('title');
+$text = get_field('description');
+$link = get_field('cta_button');
+$link2 = get_field('second_button');
+
+?>
+
+<section class="block-404">
+    <?php if($img):?>
+        <div class="bg">
+            <img src="<?= $img['url'];?>" alt="<?= $img['alt'];?>">
+        </div>
+    <?php endif;?>
+    <div class="container">
+        <div class="row">
+            <div class="content p-0">
+                <?php if($subtitle):?>
+                    <h6 class="subtitle"><?= $subtitle;?></h6>
+                <?php endif;?>
+                <?php if($title):?>
+                    <h1><?= $title;?></h1>
+                <?php endif;?>
+                <?php if($text):?>
+                    <div class="text">
+                        <?= $text;?>
+                    </div>
+                <?php endif;?>
+                <?php if( $link ):
+                    $link_url = $link['url'];
+                    $link_title = $link['title'];
+                    $link_target = $link['target'] ? $link['target'] : '_self';
+                    ?>
+                    <div class="btn-wrap d-flex flex-wrap">
+                        <a class="btn-default rounded-5 btn-block" href="<?= esc_url($link_url); ?>" target="<?= esc_attr($link_target); ?>"><?= esc_html($link_title); ?></a>
+                    </div>
+                <?php endif; ?>
+
+                <?php if( $link2 ):
+                    $link2_url = $link2['url'];
+                    $link2_title = $link2['title'];
+                    $link2_target = $link2['target'] ? $link2['target'] : '_self';
+                    ?>
+                    <div class="btn-wrap d-flex flex-wrap">
+                        <a class="btn-default rounded-5 btn-block" href="<?= esc_url($link2_url); ?>" target="<?= esc_attr($link2_target); ?>"><?= esc_html($link2_title); ?></a>
+                    </div>
+                <?php endif; ?>
+
+            </div>
+        </div>
+    </div>
+</section>
